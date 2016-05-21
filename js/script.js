98024044521e320905004ae9acecc6d6e7f494b1
Hanzhi Wang <hanzhi2@gmail.com>
2016-05-21 14:35:07 +1200

    just in case

--- js/script.js
+++ js/script.js
@@ -1,3 +1,6 @@
+var fadeTime=100;
+var currentMode='main';
+
 //Sizing and repositioning for overlay elements on top of map image on resize
 $(window).load(function(){
     $(window).resize(function(){
@@ -10,17 +13,67 @@ $(window).load(function(){
         $('.img-container').height($('#map-image').height());
     });
     $(window).resize();
-    $(window).resize();
 
+	//Set initial overlay to main
+	$('.icon-container').hide();
+	$('.icon-container.main').show();
+	
+	//Click listeners for menu bar items
+	$('.menu-bar-item').click(function(){
+		if($(this).attr('data-item')==currentMode){
+			return;
+		}
+		currentMode=$(this).data('item');
+		$('.menu-bar-item').removeClass('active');
+		$('.menu-bar-item[data-item="'+currentMode+'"]').addClass('active');
+		$('.icon-container').fadeOut(fadeTime);
+		console.log(currentMode);
+		setTimeout(function(){
+			$('.icon-container.'+currentMode).fadeIn(fadeTime);
+		},fadeTime*2);
+
+		alignArrow();
+	});
     //Toggling lights
     $('.light-icon').click(function(){
-        if($(this).hasClass('on')){
-            $(this).removeClass('on');
-            $(this).addClass('off');
-        }else{
-            $(this).removeClass('off');
-            $(this).addClass('on');
-        }
+        $(this).toggleClass('on').toggleClass('off');
     });
+	//Toggling room overview detail
+	$('.room-overview').click(function(){
+		$(this).find('.room-overview-detail').toggleClass('hidden');
+		centerElements();
+	});
+	//Toggling switch
+	$('.toggle-switch').click(function(){
+		$(this).toggleClass('off');
+	});
+
+	//Notifications button
+	$('#notifications-button,.notifications-container').click(function(){
+		$('.notifications-container').toggleClass('hidden');
+	})
+	centerElements();
+	alignArrow();
+
+	setTimeout(function(){
+		$(window).resize();
+	},100);
 });
+
+function centerElements(){
+	$('.center-align').each(function(i,el){
+		$(el).css({
+			"margin-left" :(-($(el).outerWidth()/2)),
+			"margin-top":(-($(el).outerHeight()/2))
+		});
+	});
+}
+
+function alignArrow(){
+	console.log('.icon-container.'+currentMode);
+	$('.menu-pointer').css({
+		'left': $('.menu-bar-item[data-item="'+currentMode+'"]').position().left,
+		'top': -$('.menu-bar-item[data-item="'+currentMode+'"]').height()-$('.menu-pointer').height()
+	});
+}
 
